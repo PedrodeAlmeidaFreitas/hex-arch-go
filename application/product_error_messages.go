1a80f5901b1be5f55e418fce2b7f778157af1229
Pedro de Almeida <pedro.a.freitas.1992@gmail.com>
2022-03-07 23:01:31 -0300

    Adding first model with "interface" and the tests

---
+++ application/product_error_messages.go
@@ -0,0 +1,8 @@
+package application
+
+const (
+	STATUS_INVALID = "the status must be enabled or disabled"
+	PRICE_INVALID  = "the price must be greater or equal zero"
+	ENABLE_ERROR   = "the price must be greater than zero to enable the product"
+	DISABLE_ERROR  = "the price must be zero in order to have the product disabled"
+)
---
+++ application/product_test.go
@@ -0,0 +1,60 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/pedrodealmeidafreitas/hex-arch-go/application"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, application.ENABLE_ERROR, err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, application.DISABLE_ERROR, err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, application.STATUS_INVALID, err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, application.PRICE_INVALID, err.Error())
+}
